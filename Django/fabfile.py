e6a2ecc663fc108f8f603096923f97a4274d08fd
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2014-02-21 22:23:10 +0100

    Creating a virtual environment with all requirements

--- Django/fabfile.py
+++ Django/fabfile.py
@@ -2,16 +2,19 @@ from fabric.api import local, cd, run
 
 local_dir = './'
 
+def create_environment(name='venv'):
+        local('virtualenv %s' % name)
+
 def launch(version="", port=8001):
-	local("python%s manage.py runserver %s" % (version, port))
+        local("python%s manage.py runserver %s" % (version, port))
 
 def install(version=""):
-	local("pip install south django djangorestframework django-cors-headers")
-	local("python%s manage.py syncdb" % version)
-	local("python%s manage.py schemamigration --initial app" % version)
-	local("python%s manage.py migrate rest_framework.authtoken " % version)
-	local("python%s manage.py createsuperuser" % version)
+        local("pip install south django djangorestframework django-cors-headers")
+        local("python%s manage.py syncdb" % version)
+        local("python%s manage.py schemamigration --initial app" % version)
+        local("python%s manage.py migrate rest_framework.authtoken " % version)
+        local("python%s manage.py createsuperuser" % version)
 
 def clean():
-    """Remove all the .pyc files"""
-    local("find . -name '*.pyc' -print0|xargs -0 rm", capture=False)
+        """Remove all the .pyc files"""
+        local("find . -name '*.pyc' -print0|xargs -0 rm", capture=False)
