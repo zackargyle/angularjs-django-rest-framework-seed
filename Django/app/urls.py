33961dd50912fd23c5c9384bca852a2f6727524f
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2014-02-21 22:15:20 +0100

    Reformating

--- Django/app/urls.py
+++ Django/app/urls.py
@@ -2,13 +2,18 @@ from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
 from app import views
 
-urlpatterns = patterns('app.views', url(r'^addresses$', views.AddressList.as_view(), name='address-list'),
-                       url(r'^addresses/(?P<pk>[0-9]+)$', views.AddressDetail.as_view(), name='address-detail'),
-                       url(r'^users$', views.UserList.as_view(), name='user-list'),
-                       url(r'^users/(?P<pk>[0-9]+)$', views.UserDetail.as_view(), name='user-detail'),)
+urlpatterns = patterns(
+    'app.views',
+    url(r'^addresses$', views.AddressList.as_view(), name='address-list'),
+    url(r'^addresses/(?P<pk>[0-9]+)$', views.AddressDetail.as_view(), name='address-detail'),
+    url(r'^users$', views.UserList.as_view(), name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)$', views.UserDetail.as_view(), name='user-detail'),
 
-urlpatterns += patterns('',
-    url(r'^api-token-auth/', 'rest_framework.authtoken.views.obtain_auth_token')
+    )
+
+urlpatterns += patterns(
+    '',
+    url(r'^api-token-auth/', 'rest_framework.authtoken.views.obtain_auth_token'),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
